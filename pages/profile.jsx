4a3b92c7591551bec4d542779f674b19e1c45946
Josh <31908788+truemiller@users.noreply.github.com>
2022-11-03 15:23:42 +0000

    added navbar to profile page

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,6 +1,9 @@
+import Navbar from '../components/Navbar'
+
 export default function Profile() {
   return (
     <>
+      <Navbar />
       <div className="container mx-auto">
         <h1>Profile</h1>
         <p>The user's profile will be visible here.</p>
