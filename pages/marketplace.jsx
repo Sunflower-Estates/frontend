f90de014290e7d3af7e34d32c149a5db643629f3
Josh <31908788+truemiller@users.noreply.github.com>
2022-11-03 15:19:08 +0000

    add plceholder page for marketplace

---
+++ pages/marketplace.jsx
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar'
+
+export default function Marketplace() {
+  return (
+    <>
+      <Navbar />
+      <div className="container mx-auto">
+        <h1>Marketplace</h1>
+        <p>This is where users will be able to buy/upgrade their cards.</p>
+      </div>
+    </>
+  )
+}
