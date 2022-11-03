25f702b373c515530f17eea27f36d32ba9ce2617
Josh <31908788+truemiller@users.noreply.github.com>
2022-11-03 15:20:29 +0000

    add placeholder page for profile

---
+++ pages/profile.jsx
@@ -0,0 +1,10 @@
+export default function Profile() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <h1>Profile</h1>
+        <p>The user's profile will be visible here.</p>
+      </div>
+    </>
+  )
+}
