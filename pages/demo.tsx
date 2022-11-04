e0ec54e65cefcf0e603e2c39e3241e966108ce67
Josh <31908788+truemiller@users.noreply.github.com>
2022-11-04 01:58:59 +0000

    add comments

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -47,9 +47,13 @@ export const addCardToData = (
   card: CardType,
   array: CardDataType[]
 ): CardDataType[] => {
+  // if the card data already exists
   if (array.find((cardData) => cardData.card == card)) {
+    // update the array
     return array.map((cardData) => {
+      // where target card data is found
       if (cardData.card == card) {
+        // update the card data
         return { card: card, count: cardData.count + 1 };
       } else {
         return cardData;
@@ -64,18 +68,26 @@ export const removeCardFromData = (
   card: CardType,
   array: CardDataType[]
 ): CardDataType[] => {
+  // check if the card already exists in the array
   const arrayCardData = array.find((cardData) => cardData.card == card);
   const hasCard = !!arrayCardData;
+
   if (hasCard) {
+    // make the new card data (update count)
     const newArrayCardData = {
       ...arrayCardData,
       count: arrayCardData.count - 1,
     };
+
     if (newArrayCardData.count <= 0) {
+      // remove card from array
       return array.filter((cardData) => cardData.card != newArrayCardData.card);
     } else {
+      // update the card data
       return array.map((cardData) => {
+        // where card to update is found
         if (cardData.card == newArrayCardData.card) {
+          // return the new card data
           return newArrayCardData;
         } else {
           return cardData;
@@ -87,7 +99,7 @@ export const removeCardFromData = (
   }
 };
 
-export const getCardDataLength = (array: CardDataType[]): number => {
+export const getCardDataCount = (array: CardDataType[]): number => {
   return array.reduce((a: number, b: CardDataType) => a + b.count, 0);
 };
 
@@ -231,7 +243,7 @@ export default function Demo(): JSX.Element {
         <div className="grid grid-cols-12">
           <div>
             <h2>Deck</h2>
-            <pre>{getCardDataLength(player1Deck)}</pre>
+            <pre>{getCardDataCount(player1Deck)}</pre>
           </div>
           <div className="col-span-6 grid grid-cols-6">
             <div className="col-span-full">
