3be4c2f8c3ed4780c99ffa3310d226824398a35f
Josh <31908788+truemiller@users.noreply.github.com>
2022-11-19 23:48:02 +0000

    fix: fixed sizings

--- components/Player1Area.tsx
+++ components/Player1Area.tsx
@@ -11,20 +11,15 @@ export function Player1Area() {
   const { player1, player1Hand, setPlayer1Hand, setPlayArea, player1Deck } =
     player1AreaContext;
   return (
-    <div className="grid grid-cols-12" style={{ minHeight: "230px" }}>
-      <div className="col-span-1"></div>
-      <div className="bg-white border">Resources</div>
-      <div className="bg-white border">Discard</div>
-      <div className="col-span-6 grid grid-cols-6 bg-white border">
-        <div className="col-span-full">
-          <h2>
-            Player 1: {player1.name} - {player1.gold} gold - {player1.actions}{" "}
-            actions - {player1.victory} VP
-          </h2>
-        </div>
+    <div className="flex" style={{ minHeight: "230px" }}>
+      <div className="bg-white border" style={{ width: 125 }}>
+        <img src="" alt="" />
+      </div>
+      <div className="bg-white border" style={{ width: 125 }}></div>
+      <div className=" bg-white border">
         <Player1Hand />
       </div>
-      <div className="bg-white border">
+      <div className="bg-white border" style={{ width: 125 }}>
         <h2>Deck</h2>
         <pre>{getCardDataCount(player1Deck)}</pre>
       </div>
