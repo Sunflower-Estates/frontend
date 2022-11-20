8871a1b6b8b4d42f318b144452ec39a4c6346ad9
Josh <31908788+truemiller@users.noreply.github.com>
2022-11-20 20:06:59 +0000

    feat: add holder cards for discard and trash

--- components/Areas/Player1Area.tsx
+++ components/Areas/Player1Area.tsx
@@ -12,8 +12,24 @@ export function Player1Area() {
   const { player1Deck } = player1AreaContext;
   return (
     <div className="flex" style={{ minHeight: "230px" }}>
-      <div className="bg-white border" style={{ width: 125 }}></div>
-      <div className="bg-white border" style={{ width: 125 }}></div>
+      <div
+        className=" border"
+        style={{
+          width: 125,
+          background: "url('/img/holders/Holder_Trash.png')",
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+        }}
+      ></div>
+      <div
+        className=" border"
+        style={{
+          width: 125,
+          background: "url('/img/holders/Holder_Discard_Pile.png')",
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+        }}
+      ></div>
       <div className=" bg-white border">
         <Player1Hand />
       </div>
